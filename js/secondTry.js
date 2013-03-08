f649c3a1c659453ae8558bdd55cf670b11ef82cd
Abbas Abdulmalik <abbas229@gmail.com>
2013-03-07 21:24:41 -0500

    Full Rebuild 2

--- js/secondTry.js
+++ js/secondTry.js
@@ -1,4 +1,4 @@
-objectEventHandler( window, "load", function(){
+objectEventHandler( window, "load", init);
 //==============DATA=======================
     var ajax = new HttpObject()
     , records = []
@@ -17,7 +17,6 @@ function init(){
     o("match").focus();
     ajax.open("GET", "https://dl.dropbox.com/u/21142484/_SIT213/SecondTry/docs/ComputerStudents.csv", true );
     ajax.onreadystatechange = function() {
-        try{
             if ( ajax.readyState == 4 ){
                 if ( ajax.status == 200 || ajax.status == 0 ){ //  || ajax.status == 0  
                     records = ajax.responseText.split("\n");
@@ -30,42 +29,36 @@ function init(){
                     if ( confirm("Trouble getting Data remotely.\r\rClick OK to try again.") ) init();
                 }            
             }
-        }
-        catch(err){
-            if ( confirm("ERROR getting Data remotely.\r\rClick OK to try again.") ) init();            
-        }
     };
     ajax.send();
 }
 //------------------------------------------------
-    function nowShowRecord(){
-        try{
-            var record = records[recordPointer].split(",");
-        }
-        catch(err){
-            //return; //in case records[recordPointer] is undefined and can't split()
-        }
-        try{
-            o("field0").value = record[0];
-        }
-        catch(err){}
-        for( var i = 1; i< record.length; i++ ) {
-            try{    
-                o("field"+i.toString()).value = record[i];
-            }
-            catch(err){}
-        }
-        try{
-            o("c").innerHTML = recordPointer;
-            if( matchCount != 0 ){
-                o('matchIndex').innerHTML = indexPointer +1;
-                o('sp').innerHTML = singularPlural("match", matchCount);
-            }
+function nowShowRecord(){
+    try{
+        var record = records[recordPointer].split(",");
+    }
+    catch(err){
+        //return; //in case records[recordPointer] is undefined and can't split()
+    }
+    try{
+        o("field0").value = record[0];
+    }
+    catch(err){}
+    for( var i = 1; i< record.length; i++ ) {
+        try{    
+            o("field"+i.toString()).value = " " + record[i];
         }
         catch(err){}
     }
+    try{
+        o("c").innerHTML = recordPointer;
+        if( matchCount != 0 ){
+            o('matchIndex').innerHTML = indexPointer +1;
+            o('sp').innerHTML = singularPlural("match", matchCount);
+        }
+    }
+    catch(err){}
+}
 //===============================================  
-    init();  
-    alert("okay so far");
+init();  
 //===============================================
-});// end of window load
